8b5b19a476b7af495928748edf74e0ac2654b39c
fsuettmann <fsuettmann@users.noreply.github.com>
2018-08-29 14:23:11 +0200

    mainly documentation changes

--- R/Dataset-documentation.R
+++ R/Dataset-documentation.R
@@ -1,28 +1,31 @@
-#'Parts of the ?YEAR? census in Brazil
+#'Subset of the 2010 census in Brazil
 #'
-#'The dataset contains 350,696 individual observations from the ?YEAR? census in Brazil. 
-#'Number of variables and observations are reduced to a size for a reasonable example 
-#'of our ELLsae package
+#' The dataset contains 350,696 individual observations from the 2010 census in Brazil. 
+#' Number of variables and observations are reduced to a size for a reasonable example 
+#' of the ELLsae package. The full documentation can be found here: 
+#' \url{http://microdata.worldbank.org/index.php/catalog/2078/study-description}.
 #'
 #'@format A data frame with 350696 rows and 11 variables:
 #'\describe{
 #'  \item{hh_inc}{household income}
-#'  \item{geo2_br}{Consistent level two boundaries of Brazil, Municipality 1980 - 2010}
+#'  \item{geo2_br}{inconsistent level two boundaries of Brazil, Municipality 1980 - 2010}
 #'  \item{year}{year of the census}
 #'  \item{age}{The age of a person in years}
-#'  \item{sex}{sex as factor 2 and 1}
+#'  \item{sex}{sex (discrete)}
 #'  \item{adults}{number of adults}
 #'  \item{children}{number of children}
-#'  \item{religion}{?}
-#'  \item{computer}{factor 1 or 2}
-#'  \item{trash}{?}
-#'  \item{urban}{yes or no (2 or 1)}
+#'  \item{religion}{indicates the person's religion, including "none"}
+#'  \item{computer}{computer in the household (discrete)}
+#'  \item{trash}{trash disposal (discrete)}
+#'  \item{urban}{Urban-rural status (discrete)}
 #'}
 #'@source \url{https://international.ipums.org/international/index.shtml}
 #'@docType data
 #'@keywords datasets
 #'@name brazil
 #'@usage data(brazil)
+#'@references Brazilian Institute of Geography and Statistics (IBGE). Brazil Demographic Census 2010. Rio de Janeiro, Brazil: 
+#'Brazilian Institute of Geography and Statistics (IBGE), 2012.
+#'@seealso Random data can also be generated using the \code{randomSAEdata} function contained in this package.
 #'
-# hh_inc, geo2_br, year, age, sex, adults, children, religion, computer, trash, urban
 NULL
